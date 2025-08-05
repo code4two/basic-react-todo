ae795988755aaa90863a6edfbb2a986eb3bcc637
Peter Adepoju <peteradepoju22@gmail.com>
2025-08-05 20:34:09 +0100

    added more components

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ function App() {
       <div className="font-bold flex-col items-center p-8 text-5xl md:text-6xl ">
         <h1 className="mb-8 md:mb-10">My Todos</h1>
 
-        <Todo title="TailwindCSS" text="Week 1" eniola="Delete" />
-        <Todo title="Master Js Basics" text="Week 2" eniola="Undo" />
-        <Todo title="React Basics" text="Week 3" eniola="Delete" />
-        <Todo title="React Hooks" text="Week 4" eniola="Undo" />
+        <Todo title="Learn TailwindCSS" text="Task 1" eniola="Delete" />
+        <Todo title="Master Js Basics" text="Task 2" eniola="Undo" />
+        <Todo title="Learn React Basics" text="Task 3" eniola="Delete" />
+        <Todo title="Learn React Hooks" text="Task 4" eniola="Undo" />
+        <Todo title="Learn Routing" text="Task 5" eniola="Delete" />
+        <Todo title="API Integration" text="Task 6" eniola="Undo" />
+        <Todo title="State Management" text="Task 7" eniola="Delete" />
       </div>
     </div>
   );
